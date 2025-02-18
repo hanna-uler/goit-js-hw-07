0c5b3986a2725425d536b80891c474e177eac81d
hanna-uler <hannauler@gmail.com>
2025-02-18 15:04:56 -0500

    start task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,7 @@
-
+const loginFormEl = document.querySelector(".login-form");
+loginFormEl.addEventListener("submit", event => {
+    eventPreventDefault();
+    if (event.target.email.length === 0 || event.target.password.length === 0) {
+        alert("All form fields must be filled in");
+    }
+});

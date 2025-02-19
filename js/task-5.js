74143a56dcf0f578ff0e713b3651084b92355158
hanna-uler <hannauler@gmail.com>
2025-02-19 13:28:31 -0500

    complete task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+
+const bodyEl = document.querySelector("body");
+const colorBtnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+colorBtnEl.addEventListener("click", getRandomHexColor);
+
+function getRandomHexColor(event) {
+  bodyEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+  spanEl.textContent =`#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
